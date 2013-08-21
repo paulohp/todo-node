9adb074e6f9bf716f8627717e9d2873f915a8d80
Paulo Pires <taralloqueiroiz@gmail.com>
2013-08-21 11:01:27 -0300

    iniciando migração para mysql

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var client = require('mysql');
 
-var Todo = new Schema({
-  done  :Boolean,
-  content  :String,
-  update_at: Date
+var HOST = 'localhost';
+var PORT = 3306;
+var MYSQL_USER = 'root';
+var MYSQL_PASS = '';
+var DATABASE = 'rapido_ware';
+var TABLE = 'todos';
+
+var mysql = client.createConnection({
+    host: HOST,
+    port: PORT,
+    user: MYSQL_USER,
+    password: MYSQL_PASS,
 });
 
-mongoose.model('Todo', Todo);
-mongoose.connect('mongodb://localhost/express-todo');
+
+mysql.query('CREATE DATABASE IF NOT EXISTS rapido_ware', function (err) {
+    if (err) throw err;
+    mysql.query('USE rapido_ware', function (err) {
+        if (err) throw err;
+        mysql.query('CREATE TABLE IF NOT EXISTS todos('
+            + 'id INT NOT NULL AUTO_INCREMENT,'
+            + 'PRIMARY KEY(id),'
+            + 'content VARCHAR(30),'
+            + 'done boolean not null default 0'
+            +  ')', function (err) {
+                if (err) throw err;
+            });
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,34 @@
-var mongoose = require('mongoose');
-var Todo = mongoose.model('Todo');
+var mysql = require('mysql');
+var HOST = 'localhost';
+var PORT = 3306;
+var MYSQL_USER = 'root';
+var MYSQL_PASS = '';
+var DATABASE = 'rapido_ware';
+var TABLE = 'todos';
 
+var connection = mysql.createConnection({
+    host: HOST,
+    port: PORT,
+    user: MYSQL_USER,
+    password: MYSQL_PASS,
+});
+connection.query('use ' + DATABASE);
 exports.create = function(req, res){
-  new Todo({
+  /*new Todo({
     content:req.body.content,
     update_at: Date.now()
   }).save(function(err, todo, count){
     res.redirect('/');
+  });*/
+  connection.query('insert into todos (content, done) values ("' + req.body.content + '", "' + 0 + '")',
+  function selectCb(err, results, fields) {
+      if (err) throw err;
+      else res.redirect('/');
   });
 }
 
 exports.index = function(req, res){
-  Todo.find(function(err, todos, count){
+  /*Todo.find(function(err, todos, count){
     res.render('index', 
       { 
         title: 'O que precisa ser feito?',
@@ -19,19 +36,28 @@ exports.index = function(req, res){
         count: count
       }
     );
+  });*/
+  connection.query('SELECT * FROM todos;', function (err, todos, count) {
+    res.render('index', 
+        { 
+          title: 'O que precisa ser feito?',
+          todos: todos,
+          count: count
+        }
+      );
   });
 };
 
 exports.destroy = function(req, res){
-  Todo.findById(req.params.id, function(err, todo){
+  /*Todo.findById(req.params.id, function(err, todo){
     todo.remove(function(err, todo){
       res.redirect('/');
     });
-  });
+  });*/
 }
 
 exports.update = function(req, res){
-  Todo.findById(req.params.id, function(err, todo){
+  /*Todo.findById(req.params.id, function(err, todo){
     if (req.body.done == 'on') {
       todo.done = true;
       todo.save(function(err, todo, count){
@@ -43,36 +69,36 @@ exports.update = function(req, res){
         res.redirect('/');
       });
     };
-  });
+  });*/
 }
 
 exports.done = function(req, res){
-  Todo.find({'done': true}, function(err, todos, count){
+  /*Todo.find({'done': true}, function(err, todos, count){
     res.render('index', 
       { 
         title: 'O que precisa ser feito?',
         todos: todos
       }
     );
-  });
+  });*/
 }
 
 exports.active = function(req, res){
-  Todo.find({'done': false || null}, function(err, todos, count){
+  /*Todo.find({'done': false || null}, function(err, todos, count){
     res.render('index',
       {
         title: 'O que precisa ser feito?',
         todos: todos
       }
     );
-  });
+  });*/
 }
 
 exports.destroyAll = function(req, res){
-  Todo.remove({}, function(err) { 
+  /*Todo.remove({}, function(err) { 
     res.render('index',{
       title:'O que precisa ser feito?',
       todos: null
     });
-  }); 
+  });*/ 
 }
