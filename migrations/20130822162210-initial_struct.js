b9e05d15e41059467f11b7fc611074f83cf34212
Paulo Pires <taralloqueiroiz@gmail.com>
2013-08-22 16:49:23 -0300

    primeira migration com sequelize

--- migrations/20130822162210-initial_struct.js
+++ migrations/20130822162210-initial_struct.js
@@ -1,10 +1,17 @@
 module.exports = {
   up: function(migration, DataTypes, done) {
-    // add altering commands here, calling 'done' when finished
+    migration.addColumn(
+      'Todoes',
+      'project',
+      {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    );
     done()
   },
   down: function(migration, DataTypes, done) {
-    // add reverting commands here, calling 'done' when finished
+    migration.removeColumn('Todoes', 'project');
     done()
   }
 }
